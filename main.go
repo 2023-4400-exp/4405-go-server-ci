2752a1aab6edd41bbee9988e7f83211c3e46df50
R4iw3r103 <stkin1003@gmail.com>
2023-12-19 14:05:58 +0900

    First commit

---
+++ main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+    "net/http"
+    "github.com/labstack/echo"
+)
+
+func main() {
+    e := echo.New()
+
+    e.GET("/", Hello())
+    e.GET("/api/", ApiHelloGet())
+
+    e.Start(":8080")
+}
+
+func Hello() echo.HandlerFunc {
+    return func(c echo.Context) error {
+        return c.String(http.StatusOK, "4405: Hello, GitHub Actions! ver.2")
+    }
+}
+
+func ApiHelloGet() echo.HandlerFunc {
+    return func(c echo.Context) error {
+        return c.JSON(http.StatusOK, map[string]interface{}{"studentId": "4405", "message": "Hello, GitHub Actions!"})
+    }
+}
+
